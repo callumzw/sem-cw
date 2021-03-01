b5e6a6aabf617df10aa01c08642528b586e1361f
callumzw <40447724@live.napier.ac.uk>
2021-03-01 16:54:43 +0000

    Clean Develop Branch

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,22 +8,6 @@ public class App {
         // Create new Application
         App a = new App();
 
-        // Connect to database
-        Database.connect();
-
-        //Get Angola
-        Country country = Country.getCountry("AGO");
-        //Display country info
-        Country.displayCountry(country);
-
-        //get Den Haag
-        City city = City.getCity(7);
-        City.displayCity(city);
-
-        // Disconnect from database
-        Database.disconnect();
-
 
     }
-
 }
