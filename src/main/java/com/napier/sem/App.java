f2e1749c0a747f8f780090f7a3481b551f4529a6
callumzw <40447724@live.napier.ac.uk>
2021-03-02 23:29:26 +0000

    Print cities in a district Task *Done*

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -17,9 +17,13 @@ public class App {
         //ArrayList<City> cities = City.citiesWorld();
         //City.displayCity(cities);
 
+        // Get all Cities in a District
+        ArrayList<City> cities = City.citiesDistrict("Zuid-Holland");
+        City.displayCity(cities);
+
         // Get all Capitals
-        ArrayList<Capital> capitals = Capital.capitalsWorld();
-        Capital.displayCapital(capitals);
+        // ArrayList<Capital> capitals = Capital.capitalsWorld();
+        // Capital.displayCapital(capitals);
 
 
         // Disconnecting from MySQL Database
