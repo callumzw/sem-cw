08e47fb2d581fbb981f770bdadea8b91fe60231e
callumzw <40447724@live.napier.ac.uk>
2021-03-02 23:55:03 +0000

    Print cities in a region Task *Done*

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,22 +8,27 @@ public class App {
 
         // Connecting to MySQL Database
         Connection.connect();
-
+/**
         // Get all Countries
-        //ArrayList<Country> countries = Country.countriesWorld();
-        // Country.displayCountry(countries);
+        ArrayList<Country> countries = Country.countriesWorld();
+         Country.displayCountry(countries);
 
         // Get all Cities
-        //ArrayList<City> cities = City.citiesWorld();
-        //City.displayCity(cities);
+        ArrayList<City> cities = City.citiesWorld();
+        City.displayCity(cities);
 
         // Get all Cities in a District
         ArrayList<City> cities = City.citiesDistrict("Zuid-Holland");
         City.displayCity(cities);
-
+**/
+        // Get all Cities in a Region
+        ArrayList<City> cities = City.citiesRegion("Caribbean");
+        City.displayCity(cities);
+/**
         // Get all Capitals
-        // ArrayList<Capital> capitals = Capital.capitalsWorld();
-        // Capital.displayCapital(capitals);
+         ArrayList<Capital> capitals = Capital.capitalsWorld();
+         Capital.displayCapital(capitals);
+ **/
 
 
         // Disconnecting from MySQL Database
