fafde2060e4ef2f89949b44845991000e249d73e
callumzw <40447724@live.napier.ac.uk>
2021-03-02 22:12:52 +0000

    Get all Countries Task *Done*

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,12 +1,17 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
 
         // Connecting to MySQL Database
         Connection.connect();
+
+        // Get all Countries
+        ArrayList<Country> countries = Country.countriesWorld();
+        System.out.println(countries.size());
 
         // Disconnecting from MySQL Database
         Connection.disconnect();
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,82 @@
+package com.napier.sem;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Country
+{
+    /**
+     * Country Code
+     */
+    public String code;
+
+    /**
+     * Country name
+     */
+    public String name;
+
+    /**
+     * Country Continent
+     */
+    public String continent;
+
+    /**
+     * Country Region
+     */
+    public String region;
+
+
+    /**
+     * Country Population
+     */
+    public long population;
+
+    /**
+     * Country Capital
+     */
+    public String capital;
+
+
+    /**
+     *  Methods
+     */
+
+    // All countries by Population in World
+    public static ArrayList<Country> countriesWorld()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = Connection.getCon().createStatement();
+            // Create string for SQL statement
+            String countrySelect =
+                    "SELECT Code, country.Name, Continent, Region, country.Population, city.Name "
+                            + "FROM country JOIN city ON city.CountryCode=country.Code "
+                            + "WHERE country.Capital=city.ID "
+                            + "ORDER BY country.Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(countrySelect);
+            // Return new employee if valid.
+            ArrayList<Country> countries = new ArrayList<Country>();
+            // Check one is returned
+            while (rset.next())
+            {
+                Country country = new Country();
+                country.code = rset.getString("Code");
+                country.name = rset.getString("country.Name");
+                country.continent = rset.getString("Continent");
+                country.region = rset.getString("Region");
+                country.population = rset.getLong("country.Population");
+                country.capital = rset.getString("city.Name");
+                countries.add(country);
+            }
+            return countries;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+    }
+}
