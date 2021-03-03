2bb75d661ee7283eddc17a956f3543ddadf795d0
callumzw <40447724@live.napier.ac.uk>
2021-03-03 11:21:53 +0000

    Comments Added

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,53 +8,53 @@ public class App {
 
         // Connecting to MySQL Database
         Connection.connect();
-        /**
+
         // Get all Countries
         ArrayList<Country> countries = Country.countriesWorld();Country.displayCountry(countries);
+        Country.displayCountry((countries));
 
         // Get al countries in a continent
-        ArrayList<Country> countries = Country.countriesCont("Europe");
+        countries = Country.countriesCont("Europe");
         Country.displayCountry(countries);
-         **/
+
 
         // Get all countries in a region
-        ArrayList<Country> countries = Country.countriesRegion("Central America");
+        countries = Country.countriesRegion("Central America");
         Country.displayCountry(countries);
 
-         /**
+
         // Get all Cities
         ArrayList<City> cities = City.citiesWorld();
         City.displayCity(cities);
 
         // Get all Cities in a District
-        ArrayList<City> cities = City.citiesDistrict("Zuid-Holland");
+        cities = City.citiesDistrict("Zuid-Holland");
         City.displayCity(cities);
 
         // Get all Cities in a Region
-        ArrayList<City> cities = City.citiesRegion("Caribbean");
+        cities = City.citiesRegion("Caribbean");
         City.displayCity(cities);
 
         // Get all Cities in a Continent
-        ArrayList<City> cities = City.citiesCont("Oceania");
+        cities = City.citiesCont("Oceania");
         City.displayCity(cities);
 
         // Get all Cities in a Country
-        ArrayList<City> cities = City.citiesCountry("Japan");
+        cities = City.citiesCountry("Japan");
         City.displayCity(cities);
 
 
         // Get all Capitals in the world
-         ArrayList<Capital> capitals = Capital.capitalsWorld();
-         Capital.displayCapital(capitals);
+        ArrayList<Capital> capitals = Capital.capitalsWorld();
+        Capital.displayCapital(capitals);
 
         // Get all Capitals in a Continent
-        ArrayList<Capital> capitals = Capital.capitalsCont("Europe");
+        capitals = Capital.capitalsCont("Europe");
         Capital.displayCapital(capitals);
 
         // Get all Capitals in a Region
-        ArrayList<Capital> capitals = Capital.capitalsRegion("Southern Africa");
+        capitals = Capital.capitalsRegion("Southern Africa");
         Capital.displayCapital(capitals);
-         **/
 
 
         // Disconnecting from MySQL Database
