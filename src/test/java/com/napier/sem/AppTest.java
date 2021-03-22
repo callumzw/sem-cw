7de43e9b7bcffffde6d0e505bd02c92aec910a30
Lam1412 <l.harper1412@gmail.com>
2021-03-22 21:51:53 +0000

    Added unit testing for country and city

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -3,10 +3,9 @@ package com.napier.sem;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * This class creates unit tests for the Country, City and Capital classes
  * Testing their methods validity.
