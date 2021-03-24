20a43c98587c68422aa409649dbb86fc87b2d022
callumzw <40447724@live.napier.ac.uk>
2021-03-24 08:33:18 +0000

    Full method test coverage

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.napier.sem;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -16,13 +17,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class AppIntegrationTest
 {
-    // Country Class Tests
+
     @BeforeAll
     static void init()
     {
         Connection.connect("localhost:33060");
     }
 
+    // Country tests
     @Test
     void testCountriesWorld()
     {
@@ -30,6 +32,9 @@ public class AppIntegrationTest
         assertEquals(countries.get(0).getCode(), "CHN");
         assertEquals(countries.get(0).getName(), "China");
         assertEquals(countries.get(0).getContinent(), "Asia");
+        assertEquals(countries.get(0).getRegion(), "Eastern Asia");
+        assertEquals(countries.get(0).getPopulation(), 1277558000 );
+        assertEquals(countries.get(0).getCapital(), "Peking" );
     }
     @Test
     void testCountriesCont()
@@ -37,7 +42,10 @@ public class AppIntegrationTest
         ArrayList<Country> countries = Country.countriesCont("Europe");
         assertEquals(countries.get(0).getCode(), "RUS");
         assertEquals(countries.get(0).getName(), "Russian Federation");
+        assertEquals(countries.get(0).getContinent(), "Europe");
         assertEquals(countries.get(0).getRegion(), "Eastern Europe");
+        assertEquals(countries.get(0).getPopulation(), 146934000 );
+        assertEquals(countries.get(0).getCapital(), "Moscow" );
     }
     @Test
     void testCountriesRegion()
@@ -46,6 +54,10 @@ public class AppIntegrationTest
         assertEquals(countries.get(0).getCode(), "MEX");
         assertEquals(countries.get(0).getName(), "Mexico");
         assertEquals(countries.get(0).getContinent(), "North America");
+        assertEquals(countries.get(0).getRegion(), "Central America");
+        assertEquals(countries.get(0).getPopulation(), 98881000);
+        assertEquals(countries.get(0).getCapital(), "Ciudad de México" );
+
     }
 
     //City Class Tests
@@ -56,6 +68,8 @@ public class AppIntegrationTest
         assertEquals(cities.get(0).getName(), "Mumbai (Bombay)");
         assertEquals(cities.get(0).getCountry(), "India");
         assertEquals(cities.get(0).getDistrict(), "Maharashtra");
+        assertEquals(cities.get(0).getPopulation(), 10500000);
+
     }
 
     @Test
@@ -65,6 +79,7 @@ public class AppIntegrationTest
         assertEquals(cities.get(0).getName(), "Sydney");
         assertEquals(cities.get(0).getCountry(), "Australia");
         assertEquals(cities.get(0).getDistrict(), "New South Wales");
+        assertEquals(cities.get(0).getPopulation(), 3276207);
 
     }
     @Test
@@ -74,6 +89,7 @@ public class AppIntegrationTest
         assertEquals(cities.get(0).getName(), "La Habana");
         assertEquals(cities.get(0).getCountry(), "Cuba");
         assertEquals(cities.get(0).getDistrict(), "La Habana");
+        assertEquals(cities.get(0).getPopulation(), 2256000);
 
     }
 
@@ -84,6 +100,7 @@ public class AppIntegrationTest
         assertEquals(cities.get(0).getName(), "Tokyo");
         assertEquals(cities.get(0).getCountry(), "Japan");
         assertEquals(cities.get(0).getDistrict(), "Tokyo-to");
+        assertEquals(cities.get(0).getPopulation(), 7980230);
 
     }
 
@@ -94,6 +111,7 @@ public class AppIntegrationTest
         assertEquals(cities.get(0).getName(), "Rotterdam");
         assertEquals(cities.get(0).getCountry(), "Netherlands");
         assertEquals(cities.get(0).getDistrict(), "Zuid-Holland");
+        assertEquals(cities.get(0).getPopulation(), 593321);
 
     }
 
@@ -154,6 +172,13 @@ public class AppIntegrationTest
         assertEquals(populations.get(0).getTotalPop(), 1277558000);
         assertEquals(populations.get(0).getCityPop(), 175953614);
         assertEquals(populations.get(0).getRuralPop(), 1101604386);
+    }
+
+
+    @AfterAll
+    static void Disconnect ()
+    {
+        Connection.disconnect();
     }
 
 
