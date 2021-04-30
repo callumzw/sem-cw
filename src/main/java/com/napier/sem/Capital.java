e244f1e921711a289f06a9008c03c9fd25101cbc
Lewis-Brookes <40452742@live.napier.ac.uk>
2021-04-30 17:26:11 +0100

    Adding New Country and Population Intergration Tests

--- src/main/java/com/napier/sem/Capital.java
+++ src/main/java/com/napier/sem/Capital.java
@@ -144,6 +144,45 @@ public class Capital {
         }
     }
 
+    //sort **N** amount of capital cities in a region in descending order
+    public static ArrayList<Capital> capitalsRegionWorld(String region, Integer limit)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = Connection.getCon().createStatement();
+            // Create string for SQL statement
+            String capitalSelect =
+                    "SELECT city.Name, city.Region, country.Name, city.Population "
+                            + "FROM city JOIN country ON city.CountryCode=country.Code "
+                            + "WHERE country.Capital=city.ID AND country.Region = '" +region+ "' "
+                            + "ORDER BY Population DESC "
+                            + "LIMIT '" +limit+ "'";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(capitalSelect);
+            // Return new employee if valid.
+            ArrayList<Capital> capitals = new ArrayList<Capital>();
+            // Check one is returned
+            while (rset.next())
+            {
+                Capital capital = new Capital();
+                capital.name = rset.getString("city.Name");
+                capital.country = rset.getString("country.Name");
+                capital.population = rset.getLong("city.Population");
+                capitals.add(capital);
+            }
+            return capitals;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
+
+
     // Display Capitals
     public static void displayCapital(ArrayList<Capital> capitals)
     {
