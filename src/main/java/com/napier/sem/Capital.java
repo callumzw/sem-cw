8d3514653f120053d2fe1e64c445cd7e805df31e
Mikolaj <40435378@live.napier.ac.uk>
2021-04-30 17:26:37 +0100

    Added 3 new methods

--- src/main/java/com/napier/sem/Capital.java
+++ src/main/java/com/napier/sem/Capital.java
@@ -165,4 +165,75 @@ public class Capital {
         }
         System.out.println("\n");
     }
+
+
+    public static ArrayList<Capital> TopCapitalsWorld(Integer limit)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = Connection.getCon().createStatement();
+            // Create string for SQL statement
+            String capitalSelect =
+                    "SELECT city.Name, country.Name, city.Population "
+                            + "FROM city JOIN country ON city.CountryCode=country.Code "
+                            + "WHERE country.Capital=city.ID"
+                            + "ORDER BY city.Population DESC "
+                            + "LIMIT '" +limit+ "'";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(capitalSelect);
+            // Return new employee if valid.
+            ArrayList<Capital> capitals = new ArrayList<Capital>();
+            // Check one is returned
+            while (rset.next()) {
+                Capital capital = new Capital();
+                capital.name = rset.getString("city.Name");
+                capital.country = rset.getString("country.Name");
+                capital.population = rset.getLong("city.Population");
+                capitals.add(capital);
+            }
+            return capitals;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get capital details");
+            return null;
+        }
+    }
+
+    public static ArrayList<Capital> TopCapitalsContinent(String continent, Integer limit)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = Connection.getCon().createStatement();
+            // Create string for SQL statement
+            String capitalSelect =
+                    "SELECT city.Name, country.Name, city.Population "
+                            + "FROM city JOIN country ON city.CountryCode=country.Code "
+                            + "WHERE country.Capital=city.ID AND Continent = '" +continent+ "' "
+                            + "ORDER BY city.Population DESC "
+                            + "LIMIT '" +limit+ "'";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(capitalSelect);
+            // Return new employee if valid.
+            ArrayList<Capital> capitals = new ArrayList<Capital>();
+            // Check one is returned
+            while (rset.next()) {
+                Capital capital = new Capital();
+                capital.name = rset.getString("city.Name");
+                capital.country = rset.getString("country.Name");
+                capital.population = rset.getLong("city.Population");
+                capitals.add(capital);
+            }
+            return capitals;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get capital details");
+            return null;
+        }
+    }
 }
